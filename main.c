a2b275c03eadd2e232fe903398efaada3a9085cf
Vini <vini@42sp.org.br>
2022-07-19 22:05:44 -0300

    Usuário usa o echo com vários argumentos e os imprime seguido de uma quebra de linha

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include <readline/readline.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
 	char *result;
 
 	result = readline("prompt: ");
-	if (strcmp(result, "echo") == 0)
+	if (strncmp(result, "echo", 4) == 0)
 	{
+		if (isspace(result[4]))
+			printf("%s", result + 5);
 		printf("\n");
 	}
 	return (0);
