fcc32d7d5b7f05a8dc30a28ef4a3899b3a8331a4
Jessica Fernanda <80687429+nandajfa@users.noreply.github.com>
2022-07-19 22:15:45 -0300

    Implementa o free no resultado da readline

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include <readline/readline.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
 	char *result;
 
 	result = readline("prompt: ");
-	if (strcmp(result, "echo") == 0)
+	if (result != NULL && strncmp(result, "echo", 4) == 0)
 	{
+		if (isspace(result[4]))
+			printf("%s", result + 5);
 		printf("\n");
 	}
+	if (result != NULL)
+		free(result);
 	return (0);
 }
