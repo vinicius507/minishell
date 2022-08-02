e574141b13671cda6fa3cdb72478dc54664a8a38
Vini <vini@42sp.org.br>
2022-08-02 16:33:38 -0300

    Implementa comando unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_builtin.c                                      :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/02 12:11:42 by vgoncalv          #+#    #+#             */
-/*   Updated: 2022/08/02 16:31:41 by vgoncalv         ###   ########.fr       */
+/*   Created: 2022/08/02 16:22:33 by vgoncalv          #+#    #+#             */
+/*   Updated: 2022/08/02 16:35:05 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <builtins/builtins.h>
 
-t_builtin	*get_builtin(const char *cmd)
+void	unset(char **args)
 {
-	if ((ft_strcmp(cmd, "exit") == 0))
-		return (shell_exit);
-	if ((ft_strcmp(cmd, "pwd") == 0))
-		return (pwd);
-	if ((ft_strcmp(cmd, "echo") == 0))
-		return (echo);
-	if ((ft_strcmp(cmd, "env") == 0))
-		return (env);
-	if ((ft_strcmp(cmd, "export") == 0))
-		return (export);
-	if ((ft_strcmp(cmd, "unset") == 0))
-		return (unset);
-	return (NULL);
+	while (*args != NULL)
+	{
+		unset_env(*args);
+		args++;
+	}
 }
