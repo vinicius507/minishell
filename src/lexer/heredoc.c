c7225f1668d9c27c771f3fb4f66d97f9bc581cfa
Vini <vini@42sp.org.br>
2022-08-22 06:40:15 -0300

    Conserta leak no heredoc

--- src/lexer/heredoc.c
+++ src/lexer/heredoc.c
@@ -60,13 +60,13 @@ static char	*get_content_from_pipe(int pipe_fd[2])
 	res = get_next_line(pipe_fd[0], &line);
 	while (res != END_OF_FILE)
 	{
-		content = join_line(content, line, SINGLE_QUOTE);
-		res = get_next_line(pipe_fd[0], &line);
 		if (res == GNL_ERROR)
 		{
 			free(content);
 			return (NULL);
 		}
+		content = join_line(content, line, SINGLE_QUOTE);
+		res = get_next_line(pipe_fd[0], &line);
 		if (content == NULL)
 			return (NULL);
 	}
@@ -88,14 +88,18 @@ static char	*handle_child_process_quit(int pid, int pipe_fd[2])
 	if (WIFEXITED(status))
 	{
 		g_sh.ret_code = WEXITSTATUS(status);
+		content = get_content_from_pipe(pipe_fd);
 		if (g_sh.ret_code == 0)
 		{
-			content = get_content_from_pipe(pipe_fd);
 			if (content == NULL)
-				error("error while reading from heredoc", NULL);
+				error(NULL, NULL);
 		}
 		else
+		{ 
+			free(content);
+			content = NULL;
 			g_sh.ret_code = 130;
+		}
 	}
 	close(pipe_fd[0]);
 	return (content);
