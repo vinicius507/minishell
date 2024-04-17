6010d86133470c8f4aa185e6cbb376bed1b73390
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-04-17 13:04:25 -0300

    chore: Adds compile_commands.json

--- nix/pkgs/minishell.nix
+++ nix/pkgs/minishell.nix
@@ -18,7 +18,8 @@ stdenv.mkDerivation {
   '';
   installPhase = ''
     mkdir -p $out/bin
-    mv minishell $out/bin/minishell
+    cp minishell $out/bin/minishell
+    cp compile_commands.json $out/compile_commands.json
   '';
   meta = with lib; {
     description = "A simple shell";
